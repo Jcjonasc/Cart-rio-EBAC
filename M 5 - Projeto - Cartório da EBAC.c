d0c4d0323e8df878dbb09049767b7da7a60733d6
Jcjonasc <135705806+Jcjonasc@users.noreply.github.com>
2023-06-22 04:40:40 -0300

    Opção de saída do sistema
    Inclusão da opção sair do sistema no menu principal

--- M 5 - Projeto - Cartório da EBAC.c
+++ M 5 - Projeto - Cartório da EBAC.c
@@ -134,6 +134,7 @@ int main()
 		printf("\t1 - Registrar Nomes\n");
 		printf("\t2 - Consultar Nomes\n");
 		printf("\t3 - Deletar Nomes\n\n");
+		printf("\t4 - Sair do Sistema\n\n");
 		printf("opção:");//Fim do menu
 	
 		scanf("%d", &opcao);//armazenando a escolha do usuário
@@ -152,6 +153,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Saindo do sistema, volte sempre!\n");
+			return 0;
 			break;
 			
 			default:
